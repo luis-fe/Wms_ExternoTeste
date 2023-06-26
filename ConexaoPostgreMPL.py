01b63738df9c545262f7cc051ae89a38aa86b963
luis-fe <luis_engp@hotmail.com>
2023-06-26 18:02:03 -0300

    PrimeiroCommit

---
+++ ConexaoPostgreMPL.py
@@ -0,0 +1,10 @@
+import psycopg2
+
+def conexao():
+    db_name = "Reposicao"
+    db_user = "postgres"
+    db_password = "Master@100"
+    db_host = "localhost"
+    portbanco = "5232"
+
+    return psycopg2.connect(database=db_name, user=db_user, password=db_password, host=db_host, port=portbanco)
