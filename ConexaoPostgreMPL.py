495cc54d18902416b84e52d89b4092b0d62a6ef0
luis-fe <luis_engp@hotmail.com>
2023-06-26 20:28:04 -0300

    db_name

--- ConexaoPostgreMPL.py
+++ ConexaoPostgreMPL.py
@@ -7,4 +7,4 @@ def conexao():
     db_host = "localhost"
     portbanco = "5232"
 
-    return psycopg2.connect(database=db_name, user=db_user, password=db_password, host=db_host, port=portbanco)
+    return psycopg2.connect(dbname=db_name, user=db_user, password=db_password, host=db_host, port=portbanco)
