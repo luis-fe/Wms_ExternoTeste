85db40e9ffa3b66573516ae36ed9b6cda98102a5
luis-fe <luis_engp@hotmail.com>
2023-06-27 17:20:20 -0300

    Master100

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 import pandas as pd
 from functools import wraps
+import os
 
 app = Flask(__name__)
 port = int(os.environ.get('PORT', 5000))
