21254d70cfe11476e071c0e2c0fd7f02b49de2d4
luis-fe <luis_engp@hotmail.com>
2023-06-28 09:10:19 -0300

    Master1001

--- main.py
+++ main.py
@@ -44,22 +44,6 @@ def get_usuariosRestricao():
 
     return jsonify(usuarios_data)
 
-@app.route('/api/UsuarioSenhaRestricao', methods=['GET'])
-@token_required
-def get_usuariosRestricao():
-    usuarios = Usuario.PesquisarSenha()
-
-    # Obtém os nomes das colunas
-    column_names = ['codigo', 'nome ','senha']
-
-    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
-    usuarios_data = []
-    for row in usuarios:
-        usuario_dict = dict(zip(column_names, row))
-        usuarios_data.append(usuario_dict)
-
-    return jsonify(usuarios_data)
-
 
 # Rota para atualizar um usuário pelo código
 @app.route('/api/Usuarios/<int:codigo>', methods=['POST'])
