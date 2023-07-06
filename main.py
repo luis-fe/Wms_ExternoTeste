8f3c91361c5ca6973c52e810ae27757bdad5e6a2
luis-fe <luis_engp@hotmail.com>
2023-07-06 15:56:54 -0300

    Atualizacao2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request, CORS
+from flask import Flask, render_template, jsonify, request
 import pandas as pd
 import os
 from functools import wraps
