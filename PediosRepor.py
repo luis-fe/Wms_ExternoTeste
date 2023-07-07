86b6fe135f6a4d90e2bc7d01763b834ef27ed9e7
luis-fe <luis_engp@hotmail.com>
2023-07-07 12:22:12 -0300

    Testes2

--- PediosRepor.py
+++ PediosRepor.py
@@ -41,12 +41,7 @@ def FilaPedidos():
         ' select f.codigopedido , f.vlrsugestao, f.codcliente , f.desc_cliente, f.cod_usuario, f.cidade, f.estado, '
         'datageracao, f.codrepresentante , f.desc_representante, f.desc_tiponota, condicaopgto, agrupamentopedido, situacaopedido  '
         '  from "Reposicao".filaseparacaopedidos f '
-        " union "
-        "select descricaopedido as codigopedido, 0 as vlrsugestao, 'Transf' as codcliente, 'transf' as desc_cliente, usuario as cod_usuario, 'tra' as cidade   "
-        " ,'tra', datageracao, 'tra', 'transferencia' as desc_representante,"
-        " 'transferencia' as desc_tiponota, 'transferencia' as desc_tiponota, 'transferencia' as condicaopgto,"
-        " descricaopedido as agrupamentopedido, 'transferencia' as situacaopedido from"
-        ' "Reposicao"."pedidosTransferecia"  ', conn)
+         , conn)
     pedidosku = pd.read_sql('select codpedido, sum(qtdesugerida) as qtdesugerida, sum(necessidade) as necessidade   from "Reposicao".pedidossku p  '
                             'group by codpedido ', conn)
     pedidosku.rename(columns={'codpedido': '01-CodPedido', 'qtdesugerida': '15-qtdesugerida','necessidade': '19-necessidade'}, inplace=True)
