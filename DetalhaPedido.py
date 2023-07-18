9c4d6d634f144faf69872cb301772ab4fdc50032
luis-fe <luis_engp@hotmail.com>
2023-07-18 17:20:34 -0300

    Testes2

--- DetalhaPedido.py
+++ DetalhaPedido.py
@@ -47,8 +47,8 @@ def DetalhaPedido(codPedido):
     # Agrupar os valores da col2 por col1 e concatenar em uma nova coluna
     DetalhaSku['endereco'] = DetalhaSku.groupby(['reduzido'])['endereco'].transform(lambda x: ', '.join(x))
     # Remover as linhas duplicadas
-    DetalhaSku['qtdesugerida'] = DetalhaSku.groupby('reduzido')['qtdesugerida'].transform('sum')
-    DetalhaSku['qtdrealizado'] = DetalhaSku.groupby('reduzido')['qtdrealizado'].transform('sum')
+    DetalhaSku['total'] = DetalhaSku.groupby('reduzido')['total'].transform('sum')
+    DetalhaSku['qtdrealizado'] = DetalhaSku.groupby('reduzido')['qtdrealizado'].transform('sum').round(0)
     DetalhaSku['a_concluir'] = DetalhaSku.groupby('reduzido')['a_concluir'].transform('sum')
 
     DetalhaSku['concluido_X_total'] = DetalhaSku['qtdrealizado'].astype(str) +'/'+DetalhaSku['qtdesugerida'].astype(str)
