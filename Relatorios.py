df6df2d01e29d4e6585dd6f54ac01c5b467523e1
luis-fe <luis_engp@hotmail.com>
2023-08-01 15:03:17 -0300

    atual

--- Relatorios.py
+++ Relatorios.py
@@ -169,22 +169,23 @@ def RelatorioSeparadores(itensPag, pagina):
 def RelatorioSeparadoresLimite(limite):
 
     conn = ConexaoPostgreMPL.conexao()
-    relatorio = pd.read_sql('SELECT * from "Reposicao"."ProducaoSeparadores" where ritmo is null  order by dataseparacao desc', conn)
+    relatorio = pd.read_sql('SELECT datatempo, usuario from "Reposicao"."ProducaoSeparadores" where ritmo is null  order by dataseparacao desc', conn)
     if not relatorio.empty:
         relatorio = relatorio.reset_index(drop=True)
 
         relatorio['horario'] = relatorio['datatempo'].str.slice(11, 21)
+        relatorio['data'] = relatorio['datatempo'].str.slice(0, 10)
         relatorio['horario'] = pd.to_datetime(relatorio['horario']).dt.time
         relatorio = relatorio.dropna(subset=['horario'])
 
         # Ordene o DataFrame pelo usuario, data e horario
-        relatorio.sort_values(by=['usuario', 'dataseparacao', 'horario'], inplace=True)
+        relatorio.sort_values(by=['usuario', 'data', 'horario'], inplace=True)
 
         def horario_centecimal(time):
             return time.hour + (time.minute / 60) + (time.second / 3600)
 
         relatorio['horario'] = relatorio['horario'].apply(horario_centecimal)
-        relatorio['ritmo'] = relatorio.groupby(['usuario', 'dataseparacao'])['horario'].diff()
+        relatorio['ritmo'] = relatorio.groupby(['usuario', 'data'])['horario'].diff()
         relatorio['ritmo'] = relatorio['ritmo'] * 3600
 
         # Remova esta linha, pois o ritmo já foi calculado corretamente
