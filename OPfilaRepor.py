69da47d1302b623baa74a5ad7d2efe7fd0461dda
luis-fe <luis_engp@hotmail.com>
2023-08-02 16:10:18 -0300

    atual

--- OPfilaRepor.py
+++ OPfilaRepor.py
@@ -44,14 +44,9 @@ def ProdutividadeRepositores(dataInicial = '0', dataFInal ='0'):
             'group by usuario ', conn, params=(dataInicial, dataFInal,))
         TagReposicao = TagReposicao.sort_values(by='qtde', ascending=False)
 
-        def format_with_separator2(value):
-            return locale.format('%0.0f', value, grouping=True)
 
-            # Aplicar a função na coluna do DataFrame
-        TagReposicao['qtde'] = TagReposicao['qtde'].apply(format_with_separator2)
         total = TagReposicao['qtde'].sum()  # Formula do valor Total
-        #total = str(total)
-        #total = total.replace(',','.')
+
 
 
         Usuarios = pd.read_sql('Select codigo as usuario, nome from "Reposicao".cadusuarios ',conn)
