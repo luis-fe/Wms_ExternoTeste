8d6dd22a7a36a504e79dd07023ce0c5bc647969d
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2023-08-02 16:06:57 -0300

    Update Produtividade.js

--- static/js/Produtividade.js
+++ static/js/Produtividade.js
@@ -1,3 +1,6 @@
+
+let qtdSugerida = 0;
+let valorSugerido = 0;
 function criarTabelasProdutividade(listaDados, tabela) {
     const tabelaProdutividade = document.getElementById(tabela);
     tabelaProdutividade.innerHTML = '';
@@ -46,10 +49,63 @@ function criarTabelasProdutividade(listaDados, tabela) {
     });
   }
   
+  function formatarMoeda(valor) {
+    const formatoMoeda = {
+      style: "currency",
+      currency: "BRL"
+    };
+        return valor.toLocaleString("pt-BR", formatoMoeda);}
+
+const ApiDistribuicao = 'http://192.168.0.183:5000/api/FilaPedidos';
+
+function DadosRetorna (){
+    fetch(ApiDistribuicao, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'a40016aabcx9'
+        },
+      })
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            alert("Erro na Atualização, Recarregue a página!\nSe o problema persistir, contate o Administrador!");
+          }
+        })
+        .then(data => {
+          const TipoNotaFiltrado = data.filter(item => item["03-TipoNota"] !== "39 - BN MPLUS");
+          const Filtro2 = TipoNotaFiltrado.filter(item => item["22- situacaopedido"] !== "Em Conferencia");
+          Filtro2.forEach(item => {
+            item["12-vlrsugestao"] = item["12-vlrsugestao"].replace("R$", "");
+            item["12-vlrsugestao"] = parseFloat(item["12-vlrsugestao"]).toFixed(2);
+    
+            qtdSugerida += item["15-qtdesugerida"];
+            valorSugerido += parseFloat(item["12-vlrsugestao"]); // Converte para número antes de acumular
+          });
+    
+          document.getElementById("RetornaPcs").textContent = qtdSugerida;
+          document.getElementById("RetornaValor").textContent = formatarMoeda(valorSugerido); // Formata o valor após a soma
+          qtdSugerida = 0;
+          valorSugerido = 0;
+    
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+
+
+    function getFormattedDate(date) {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
+      }
 
 
 
-function tabelaProdutividade (Consulta, Tabela, dataIni, DataFim){
+function tabelaProdutividade (Consulta, Tabela, dataIni, DataFim, NomeRecorde, QtdRecorde, Qtd){
     fetch (`http://192.168.0.183:5000/api/${Consulta}/Resumo?DataInicial=${dataIni}&DataFinal==${DataFim}`,{
         method: 'GET',
             headers: {
@@ -61,12 +117,18 @@ function tabelaProdutividade (Consulta, Tabela, dataIni, DataFim){
                 if (response.ok) {
                     return response.json();
                 } else {
-                    throw new Error('Erro ao atribuir pedidos');
+                    alert("Erro na Atualização, Recarregue a página!\nSe o problema persistir contacte o Administrador!")
                 }
             })
             .then(data => {
-                console.log(data);
-                criarTabelasProdutividade(data, Tabela);
+                const detalhamentoRanking = data[0]['3- Ranking Repositores'];
+                const detalhamentoNomeRecord = data[0]["1- Record Repositor"];
+                const detalhamentoQtdRecord = data[0]["1.1- Record qtd"];
+                const Total = data[0]["2 Total Periodo"];
+                criarTabelasProdutividade(detalhamentoRanking, Tabela);
+                document.getElementById(NomeRecorde).textContent = detalhamentoNomeRecord;
+                document.getElementById(QtdRecorde).textContent = `${detalhamentoQtdRecord} Pçs`
+                document.getElementById(Qtd).textContent = `${Total} Pçs`;
                 
             })
             .catch(error => {
@@ -87,7 +149,7 @@ const botaoFiltrar = document.getElementById("btnFiltrar")
 
 botaoFiltrar.addEventListener('click', () => {
     atualizarTabelas();
-
+    document.getElementById("btnFiltrar").disabled = true;
 })
 
       
@@ -95,8 +157,9 @@ botaoFiltrar.addEventListener('click', () => {
         const currentDate = new Date();
         const formattedDate = getFormattedDate(currentDate);
       
-        tabelaProdutividade("TagsReposicao", "TAbelaRepositor", formattedDate, formattedDate);
-        tabelaProdutividade("TagsSeparacao", "TAbelaSeparador", formattedDate, formattedDate);
+        tabelaProdutividade("TagsReposicao", "TAbelaRepositor", formattedDate, formattedDate, "NomeRecordeRep", "ValorRecordeRep", "LabelTotalReposto");
+        tabelaProdutividade("TagsSeparacao", "TAbelaSeparador", formattedDate, formattedDate, "NomeRecordeSep", "ValorRecordeSep", "LabelTotalSeparado");
+        DadosRetorna();
 
         document.getElementById("DataInicial").value = formattedDate
         document.getElementById("DataFinal").value = formattedDate
@@ -107,24 +170,21 @@ botaoFiltrar.addEventListener('click', () => {
     function atualizarTabelas() {
         const DataIni = document.getElementById("DataInicial").value;
         const DataFin = document.getElementById("DataFinal").value;
-
-        console.log(DataIni);
-        console.log(DataFin);
       
         Promise.all([
-          tabelaProdutividade("TagsReposicao", "TAbelaRepositor", DataIni, DataFin),
-          tabelaProdutividade("TagsSeparacao", "TAbelaSeparador", DataIni, DataFin)
+          tabelaProdutividade("TagsReposicao", "TAbelaRepositor", DataIni, DataFin, "NomeRecordeRep", "ValorRecordeRep", "LabelTotalReposto"),
+          tabelaProdutividade("TagsSeparacao", "TAbelaSeparador", DataIni, DataFin, "NomeRecordeSep", "ValorRecordeSep", "LabelTotalSeparado")
         ]).then(() => {
           criarTabelasProdutividade(dataReposicao, "TAbelaRepositor");
           criarTabelasProdutividade(dataSeparacao, "TAbelaSeparador");
-      
-          // Agendar a próxima atualização após 1 minuto
-          setTimeout(atualizarTabelas, 60000);
+          document.getElementById("btnFiltrar").disabled = false;
         }).catch(error => {
           console.error(error);
-          // Em caso de erro, agendar a próxima atualização após 1 minuto
-          setTimeout(atualizarTabelas, 60000);
+          document.getElementById("btnFiltrar").disabled = false; // Habilita o botão mesmo em caso de erro
         });
+      
+        DadosRetorna(); // Chama a função DadosRetorna() para atualizar os dados
+        setTimeout(atualizarTabelas, 60000); // 60000 ms = 1 minuto
       }
 
       
