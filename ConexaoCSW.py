c62e0228d9f3d2a1dc6387f5a9e08c67b7ed6800
luis-fe <luis_engp@hotmail.com>
2023-08-30 16:03:35 -0300

    atual

--- ConexaoCSW.py
+++ ConexaoCSW.py
@@ -6,7 +6,7 @@ def Conexao():
     conn = jaydebeapi.connect(
     'com.intersys.jdbc.CacheDriver',
     'jdbc:Cache://192.168.0.25:1972/CONSISTEM',
-    {'user': '_admin', 'password': 'ccscache'},
+    {'user': '_system', 'password': 'ccscache'},
     'CacheDB.jar'
 )
     return conn
