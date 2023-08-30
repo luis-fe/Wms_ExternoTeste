a09b21618b4f5b3d1ce1e54ec99e59974b6ad831
luis-fe <luis_engp@hotmail.com>
2023-08-30 20:53:30 -0300

    atual

--- PediosApontamento.py
+++ PediosApontamento.py
@@ -111,7 +111,7 @@ def FilaPedidos():
     pedido['21-MARCA'].fillna('-', inplace=True)
     pedido['22- situacaopedido'].fillna('No Retorna', inplace=True)
     pedido.fillna('-', inplace=True)
-    pedido = pedido.iloc[0:200]
+    pedido = pedido.iloc[0:180]
 
 
     return pedido
