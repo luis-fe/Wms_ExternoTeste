db103f178b11e198dd31ade834d9fab63b63f263
luis-fe <luis_engp@hotmail.com>
2023-09-01 23:25:12 -0300

    atual

--- src/routes/usuarios.py
+++ src/routes/usuarios.py
@@ -1,4 +1,4 @@
-from src.Service import usuariosCad
+from src.Service import usuariosModel
 from flask import Blueprint, jsonify, request
 from functools import wraps
 
@@ -17,7 +17,7 @@ def token_required(f): # TOKEN FIXO PARA ACESSO AO CONTEUDO
 @usuarios_routes.route('/api/Usuarios', methods=['GET'])
 @token_required
 def get_usuarios():
-    usuarios = usuariosCad.PesquisarUsuarios()
+    usuarios = usuariosModel.PesquisarUsuarios()
     # Obtém os nomes das colunas
     column_names = ['codigo', 'nome', 'funcao', 'situacao']
     # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
@@ -26,3 +26,103 @@ def get_usuarios():
         usuario_dict = dict(zip(column_names, row))
         usuarios_data.append(usuario_dict)
     return jsonify(usuarios_data)
+
+@app.route('/api/UsuarioSenhaRestricao', methods=['GET'])
+@token_required
+def get_usuariosRestricao():
+    usuarios = usuariosModel.PesquisarSenha()
+
+    # Obtém os nomes das colunas
+    column_names = ['codigo', 'nome ', 'senha']
+
+    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
+    usuarios_data = []
+    for row in usuarios:
+        usuario_dict = dict(zip(column_names, row))
+        usuarios_data.append(usuario_dict)
+
+    return jsonify(usuarios_data)
+@app.route('/api/Usuarios/<int:codigo>', methods=['POST'])
+@token_required
+def update_usuario(codigo):
+    # Obtém os dados do corpo da requisição (JSON)
+    data = request.get_json()
+    # Verifica se a coluna "funcao" está presente nos dados recebidos
+    nome_ant, funcao_ant, situacao_ant = usuariosModel.PesquisarUsuariosCodigo(codigo)
+    if 'funcao' in data:
+        nova_funcao = data['funcao']
+    else:
+        nova_funcao = funcao_ant
+    if 'nome' in data:
+        nome_novo = data['nome']
+    else:
+        nome_novo = nome_ant
+    if 'situacao' in data:
+        situacao_novo = data['situacao']
+    else:
+        situacao_novo = situacao_ant
+    usuariosModel.AtualizarInformacoes(nome_novo, nova_funcao, situacao_novo, codigo)
+
+    return jsonify({'message': f'Dados do Usuário {codigo} - {nome_novo} atualizado com sucesso'})
+
+
+@app.route('/api/Usuarios', methods=['PUT'])
+@token_required
+def criar_usuario():
+    # Obtenha os dados do corpo da requisição
+    novo_usuario = request.get_json()
+    # Extraia os valores dos campos do novo usuário
+    codigo = novo_usuario.get('codigo')
+    funcao = novo_usuario.get('funcao')
+    nome = novo_usuario.get('nome')
+    senha = novo_usuario.get('senha')
+    situacao = novo_usuario.get('situacao')
+    # inserir o novo usuário no banco de dados
+    c, n, f = usuariosModel.PesquisarUsuariosCodigo(codigo)
+    if c != 0:
+        return jsonify({'message': f'Novo usuário:{codigo}- {nome} ja existe'}), 201
+    else:
+        usuariosModel.InserirUsuario(codigo, funcao, nome, senha, situacao)
+        # Retorne uma resposta indicando o sucesso da operação
+        return jsonify({'message': f'Novo usuário:{codigo}- {nome} criado com sucesso'}), 201
+
+
+# Rota com parametros para check do Usuario e Senha
+@app.route('/api/UsuarioSenha', methods=['GET'])
+@token_required
+def check_user_password():
+    # Obtém o código do usuário e a senha dos parâmetros da URL
+    codigo = request.args.get('codigo')
+    senha = request.args.get('senha')
+
+    # Verifica se o código do usuário e a senha foram fornecidos
+    if codigo is None or senha is None:
+        return jsonify({'message': 'Código do usuário e senha devem ser fornecidos.'}), 400
+
+    # Consulta no banco de dados para verificar se o usuário e senha correspondem
+    result = usuariosModel.ConsultaUsuarioSenha(codigo, senha)
+
+
+    # Verifica se o usuário existe
+    if result == 1:
+        # Consulta no banco de dados para obter informações adicionais do usuário
+
+        nome, funcao, situacao, empresa1 = usuariosModel.PesquisarUsuariosCodigo(codigo)
+
+        # Verifica se foram encontradas informações adicionais do usuário
+        if nome != 0:
+            usuariosModel.RegistroLog(codigo)
+            # Retorna as informações adicionais do usuário
+            return jsonify({
+                "status": True,
+                "message": "Usuário e senha VALIDADOS!",
+                "nome": nome,
+                "funcao": funcao,
+                "situacao": situacao,
+                "empresa":empresa1
+            })
+        else:
+            return jsonify({'message': 'Não foi possível obter informações adicionais do usuário.'}), 500
+    else:
+        return jsonify({"status": False,
+                        "message": f'Usuário ou senha não existe'}), 401
