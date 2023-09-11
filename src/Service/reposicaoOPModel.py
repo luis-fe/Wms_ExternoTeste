4e7c9b8af02d6c27799c9ef9c60f11f3dd7cb93b
luis-fe <luis_engp@hotmail.com>
2023-09-11 17:22:33 -0300

    atualizacao Sistemas

--- src/Service/reposicaoOPModel.py
+++ src/Service/reposicaoOPModel.py
@@ -12,7 +12,7 @@ def FilaPorOP(natureza, codempresa):
 
     df_OP_Iniciada =pd.read_sql(
         ' select "numeroop", count("numeroop") as qtdpeçs_reposto  from "Reposicao"."tagsreposicao" frt '
-        ' where empresa = %s '
+        ' where codempresa = %s '
         ' group by "numeroop" ', conn, params = (codempresa,))
     df_OP1 = pd.merge(df_OP1, df_OP_Iniciada, on='numeroop', how='left')
 
