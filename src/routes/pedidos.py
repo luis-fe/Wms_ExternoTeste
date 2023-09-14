f91aaeabc1879646f36b07f2d82611ccb19894d6
luis-fe <luis_engp@hotmail.com>
2023-09-14 15:50:42 -0300

    atualizacao Sistemas

--- src/routes/pedidos.py
+++ src/routes/pedidos.py
@@ -161,13 +161,6 @@ def imprimirEtiqueta():
     TagReposicao = imprimirEtiquetaModel.criar_pdf(f'impressao.pdf', cliente, codcliente, pedido, transportadora)
     imprimirEtiquetaModel.imprimir_pdf(f'impressao.pdf')
 
-    # Obtém os nomes das colunas
-    column_names = TagReposicao.columns
-    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
-    pedidos_data = []
-    for index, row in TagReposicao.iterrows():
-        pedidos_dict = {}
-        for column_name in column_names:
-            pedidos_dict[column_name] = row[column_name]
-        pedidos_data.append(pedidos_dict)
-    return jsonify(pedidos_data)
+
+
+    return jsonify(f"imprimido pedido {pedido}")
