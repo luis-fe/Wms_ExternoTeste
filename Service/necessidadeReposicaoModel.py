71dbcfb86233ea547499eead8e7dc77dee6c3a32
luis-fe <luisengp@gmail.com>
2023-09-23 12:42:00 -0300

    atualiza necessidades

--- Service/necessidadeReposicaoModel.py
+++ Service/necessidadeReposicaoModel.py
@@ -6,11 +6,11 @@ def RelatorioNecessidadeReposicao():
     conn = ConexaoPostgreMPL.conexao()
     relatorioEndereço = pd.read_sql(
         'select produto as codreduzido , sum(necessidade) as "Necessidade p/repor", count(codpedido) as "Qtd_Pedidos que usam"  from "Reposicao".pedidossku p '
-        "where necessidade > 0 and endereco = 'Não Reposto' and natureza = '5' "
+        "where necessidade > 0 and endereco = 'Não Reposto' and codnaturezaatual = '5' "
         " group by produto ", conn)
     relatorioEndereçoEpc = pd.read_sql(
         'select codreduzido , max(epc) as epc_Referencial, engenharia, count(codreduzido) as saldoFila from "Reposicao".filareposicaoportag f '
-        "where engenharia is not null and natureza = '5' "
+        "where engenharia is not null and codnaturezaatual = '5' "
         'group by codreduzido, engenharia ', conn)
 
     OP = pd.read_sql('select f.codreduzido, numeroop as ops, count(codreduzido) as qtde '
