cb7d7904b4cc1ad1039af87c2126b75f85590a06
luis-fe <luisengp@gmail.com>
2023-09-23 13:29:39 -0300

    collection

--- Service/chamadosModel.py
+++ Service/chamadosModel.py
@@ -5,5 +5,7 @@ def Obter_chamados():
     conn = ConexaoPostgreMPL.conexao()
     query = pd.read_sql('select * from "Reposicao".chamados '
                         'order by data_chamado', conn)
+    query.fillna('-', inplace=True)
+
     conn.close()
     return query
