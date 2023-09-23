473d5067088032d011cfc594bba31d91b90be30d
luis-fe <luisengp@gmail.com>
2023-09-23 14:28:49 -0300

    collection

--- Service/chamadosModel.py
+++ Service/chamadosModel.py
@@ -3,7 +3,7 @@ import ConexaoPostgreMPL
 
 def Obter_chamados():
     conn = ConexaoPostgreMPL.conexao()
-    query = pd.read_sql('select id_chamado, solicitante, data_chamado'
+    query = pd.read_sql('select id_chamado, solicitante, data_chamado, '
                         ' tipo_chamado, atribuido_para, descricao_chamado, status_chamado, '
                         'data_finalizacao_chamado from "Reposicao".chamados '
                         'order by data_chamado', conn)
@@ -19,6 +19,17 @@ def novo_chamados(solicitante, data_chamado, tipo_chamado, atribuido_para, descr
                    'descricao_chamado, status_chamado, data_finalizacao_chamado) '
                    'VALUES (%s, %s, %s, %s, %s, %s, %s)',(solicitante, data_chamado, tipo_chamado, atribuido_para, descricao_chamado, status_chamado, data_finalizacao_chamado))
 
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return True
+
+def encerrarchamado(id_chamado, data_finalizacao_chamado):
+    conn = ConexaoPostgreMPL.conexao()
+    cursor = conn.cursor()
+    cursor.execute('UPDATE "Reposicao"."chamados" '
+                   'SET data_finalizacao_chamado = %s'
+                   ' WHERE id_chamado = %s')
     conn.commit()
     cursor.close()
     conn.close()
