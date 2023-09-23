370c12176848adf8314b84c2c14ab9d8c6a8b140
luis-fe <luisengp@gmail.com>
2023-09-23 12:33:59 -0300

    atualiza necessidades

--- Service/necessidadeReposicaoModel.py
+++ Service/necessidadeReposicaoModel.py
@@ -25,7 +25,7 @@ def RelatorioNecessidadeReposicao():
     OP['ops'] = OP['ops'] + ':' + OP['qtde']+' Pçs'
 
     # Agrupar os valores da coluna 'novaColuna' com base na coluna 'reduzido'
-    OP_ag = OP.groupby('codreduzido')['op'].apply(lambda x: ', '.join(x)).reset_index()
+    OP_ag = OP.groupby('codreduzido')['ops'].apply(lambda x: ', '.join(x)).reset_index()
 
     relatorioEndereço = pd.merge(relatorioEndereço, relatorioEndereçoEpc, on='codreduzido', how='left')
     relatorioEndereço = pd.merge(relatorioEndereço, OP_ag, on='codreduzido', how='left')
