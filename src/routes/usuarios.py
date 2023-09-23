7c37b41fd078a5e7d6dab270b368c403888dfe92
luis-fe <luisengp@gmail.com>
2023-09-23 08:38:33 -0300

    alteracao no Index.htmlc

--- src/routes/usuarios.py
+++ src/routes/usuarios.py
@@ -2,7 +2,7 @@
 from flask import Blueprint, jsonify, request
 from functools import wraps
 
-from src.Service import usuariosModel
+import usuariosModel
 
 usuarios_routes = Blueprint('usuarios', __name__)
 
