603d7169c66239d0cda3a18bb4ea798bd709544e
luis-fe <luisengp@gmail.com>
2023-09-23 09:57:10 -0300

    alteracao no Index.htmlc

--- ConexaoCSW.py
+++ ConexaoCSW.py
@@ -19,13 +19,26 @@ def Conexao2():
     'CacheDB.jar'
 )
     return conn
+def ConexaoExterna2():
+    conn = jaydebeapi.connect(
+    'com.intersys.jdbc.CacheDriver',
+    'jdbc:Cache://187.32.10.129:1972/CONSISTEM',
+    {'user': 'root', 'password': 'ccscache'},
+    'CacheDB.jar'
+)
+    return conn
 
 def obter_notaCsw():
     try:
         conn = Conexao()
     except:
-        conn = Conexao2()
-        print('usado a conexao 2 root')
+        try:
+            conn = Conexao2()
+            print('usado a conexao 2 root')
+        except:
+            conn = ConexaoExterna2()
+            print('usado a conexao EXTERNA root')
+
 
     data = pd.read_sql(" select t.codigo ,t.descricao  from Fat.TipoDeNotaPadrao t ", conn)
     conn.close()
