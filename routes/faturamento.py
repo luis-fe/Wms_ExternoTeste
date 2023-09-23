654a3bbf9677daa8c7a52af5b525fff6f1708409
luis-fe <luisengp@gmail.com>
2023-09-23 09:07:52 -0300

    alteracao no Index.htmlc

--- routes/faturamento.py
+++ routes/faturamento.py
@@ -36,3 +36,22 @@ def get_Faturamento():
         pedidos_data.append(pedidos_dict)
     return jsonify(pedidos_data)
 
+@faturamento_routes.route('/api/ObterNotasCsw', methods=['GET'])
+@token_required
+def ObterNotasCsw():
+    # Obtém os valores dos parâmetros DataInicial e DataFinal, se estiverem presentes na requisição
+    empresa = request.args.get('empresa', '1')
+    TagReposicao = FaturamentoCswModel.ObterTipoNota(empresa)
+
+
+    # Obtém os nomes das colunas
+    column_names = TagReposicao.columns
+    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
+    pedidos_data = []
+    for index, row in TagReposicao.iterrows():
+        pedidos_dict = {}
+        for column_name in column_names:
+            pedidos_dict[column_name] = row[column_name]
+        pedidos_data.append(pedidos_dict)
+    return jsonify(pedidos_data)
+
