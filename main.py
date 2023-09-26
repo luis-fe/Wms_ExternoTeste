4b46a5ce5810f24a1f9aa2d8570212f88edb187d
luis-fe <luisengp@gmail.com>
2023-09-26 16:26:10 -0300

    correcao Schema chamado

--- main.py
+++ main.py
@@ -168,6 +168,7 @@ def get_RelatorioFila():
 UPLOAD_FOLDER = 'imagens_chamado'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route('/api/get_image/<string:idchamado>', methods=['GET'])
+@token_required
 def get_image(idchamado):
     filename = idchamado
     return send_from_directory(f'imagens_chamado/{idchamado}', filename)
