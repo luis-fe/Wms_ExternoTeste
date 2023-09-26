606cf4f231e951dbdf0ab24ef162626a08232d25
luis-fe <luisengp@gmail.com>
2023-09-26 14:21:33 -0300

    correcao Schema chamado

--- routes/chamados.py
+++ routes/chamados.py
@@ -136,7 +136,7 @@ def upload_image(idchamado):
 
     return jsonify({'message': 'Arquivo enviado com sucesso'}), 201
 
-@chamados_routes.route('/pcp/api/get_image/<string:idchamado>', methods=['GET'])
+@chamados_routes.route('/api/get_image/<string:idchamado>', methods=['GET'])
 def get_image(idchamado):
     filename = id
     return send_from_directory(f'uploads/{idchamado}', filename)
