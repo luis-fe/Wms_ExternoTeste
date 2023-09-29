c410773ce598923aa6868626bc9715a014351dc5
luis-fe <luisengp@gmail.com>
2023-09-29 18:42:40 -0300

    nova api f

--- Service/AutomacaoWMS_CSW/ReservaEnderecos.py
+++ Service/AutomacaoWMS_CSW/ReservaEnderecos.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import ConexaoPostgreMPL
 from psycopg2 import sql
-
+import datetime
+import pytz
+def obterHoraAtual():
+    fuso_horario = pytz.timezone('America/Sao_Paulo')  # Define o fuso horário do Brasil
+    agora = datetime.datetime.now(fuso_horario)
+    hora_str = agora.strftime('%d/%m/%Y %H:%M')
+    return hora_str
 
 
 
@@ -56,9 +62,13 @@ def LimparReservaPedido(pedido):
 
 def AtribuirReserva(pedido, natureza):
     conn = ConexaoPostgreMPL.conexao()
-    # Passo 1 :  obter os skus do pedido
+    total = 0  # Para Totalizar o numer de atualizcoes
+    inseridosDuplos = 0
 
-    queue = pd.read_sql('select produto from "Reposicao".pedidossku '
+    # Passo 1 :  obter os skus do pedido
+    LimparReservaPedido(pedido)
+
+    queue = pd.read_sql('select produto, necessidade from "Reposicao".pedidossku '
                         "where necessidade > 0 and codpedido = %s ",conn,params=(pedido,))
 
     enderecosSku = pd.read_sql(
@@ -67,10 +77,101 @@ def AtribuirReserva(pedido, natureza):
 
     enderecosSku = pd.merge(enderecosSku,queue, on= 'produto')
 
+    # Calculando a necessidade de cada endereco
+
+    enderecosSku['repeticoessku'] = enderecosSku.groupby('produto').cumcount() + 1
+    for i in range(0):
+        pedidoskuIteracao = enderecosSku[enderecosSku['repeticoessku'] == (i + 1)]
+
+        tamanho = pedidoskuIteracao['produto'].size
+        pedidoskuIteracao = pedidoskuIteracao.reset_index(drop=False)
+
+        for i in range(tamanho):
+            necessidade = pedidoskuIteracao['necessidade'][i]
+            saldoliq = pedidoskuIteracao['SaldoLiquid'][i]
+            endereco = pedidoskuIteracao['codendereco2'][i]
+            produto = pedidoskuIteracao['produto'][i]
+            pedido = pedidoskuIteracao['codpedido'][i]
+
+            if necessidade<= saldoliq:
+                    update = 'UPDATE "Reposicao".pedidossku '\
+                             'SET endereco = %s , reservado = %s'\
+                             'WHERE codpedido = %s AND produto = %s and reservado = %s '
+
+                    # Filtrar e atualizar os valores "a" para "aa"
+                    pedidoskuIteracao.loc[(pedidoskuIteracao['codendereco2'] == endereco) &
+                                          (pedidoskuIteracao['produto'] == produto), 'SaldoLiquid'] \
+                        = pedidoskuIteracao['SaldoLiquid'][i] - pedidoskuIteracao['necessidade'][i]
+
+                    cursor = conn.cursor()
+
+                    # Executar a atualização na tabela "Reposicao.pedidossku"
+                    cursor.execute(update,
+                                   (endereco,'sim',
+                                    pedido, produto,'nao')
+                                    )
+
+                    # Confirmar as alterações
+                    conn.commit()
+
+                    total = total + 1
+
+            elif saldoliq >0 and necessidade > saldoliq:
+                qtde_sugerida = pd.read_sql('select qtdesugerida from "Reposicao".pedidossku '
+                                            "where reservado = 'nao' and codpedido = "+"'"+pedido+"' and produto ="
+                                                                                                  " '"+produto+"'",conn)
+                if not qtde_sugerida.empty:
+                    qtde_sugerida = qtde_sugerida['qtdesugerida'][0]
+                    qtde_sugerida2 = qtde_sugerida - saldoliq
+
+                    insert = 'insert into "Reposicao".pedidossku (codpedido, datahora, endereco, necessidade, produto, qtdepecasconf, ' \
+                             'qtdesugerida, reservado, status, valorunitarioliq) ' \
+                             'select codpedido, datahora, %s, %s, produto, qtdepecasconf, ' \
+                             '%s, %s, status, valorunitarioliq from "Reposicao".pedidossku ' \
+                             'WHERE codpedido = %s AND produto = %s and reservado = %s ' \
+                             ' limit 1;'
+                    cursor = conn.cursor()
+
+                    # Executar a atualização na tabela "Reposicao.pedidossku"
+                    cursor.execute(insert,
+                                   ('Não Reposto', qtde_sugerida2, qtde_sugerida2, 'nao',
+                                    pedido, produto, 'nao')
+                                   )
+
+                    # Confirmar as alterações
+                    conn.commit()
+
+                    update = 'UPDATE "Reposicao".pedidossku ' \
+                             'SET endereco = %s , qtdesugerida = %s , reservado = %s, necessidade = %s ' \
+                             'WHERE codpedido = %s AND produto = %s and reservado = %s and qtdesugerida = %s'
+
+                    # Filtrar e atualizar os valores "a" para "aa"
+                    pedidoskuIteracao.loc[(pedidoskuIteracao['codendereco2'] == endereco) &
+                                          (pedidoskuIteracao['codreduzido'] == produto), 'SaldoLiquid'] = 0
+                    cursor = conn.cursor()
+
+                    # Executar a atualização na tabela "Reposicao.pedidossku"
+                    cursor.execute(update,
+                                   (endereco, saldoliq, 'sim', saldoliq,
+                                    pedido, produto, 'nao', qtde_sugerida)
+                                   )
+
+                    # Confirmar as alterações
+                    conn.commit()
+
+                    inseridosDuplos = 1 + inseridosDuplos
+
+            else:
+                encerra = i
+    datahora = obterHoraAtual()
+    print(f'{total} atualizacoes realizadas, as {datahora}')
+    return pd.DataFrame([{'Mensagem': f'foram reservados  {total} pçs e incrementado {inseridosDuplos}'}])
 
 
-    conn.close()
-    return enderecosSku
+
+
+
+
 
 
 
