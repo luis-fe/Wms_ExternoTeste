64c146c4d7cf0493bfda95c301c7eda4263eabb9
luis-fe <luisengp@gmail.com>
2023-10-04 17:48:07 -0300

    relatorio consumo caixas

--- Service/imprimirEtiquetaModel.py
+++ Service/imprimirEtiquetaModel.py
@@ -33,7 +33,7 @@ def criar_pdf(saida_pdf, titulo, cliente, pedido, transportadora, separador, agr
         qr.make(fit=True)
         qr_img = qr.make_image(fill_color="black", back_color="white")
         qr_img.save(qr_filename)  # Salvar a imagem do QR code no arquivo temporário
-        c.drawImage(qr_filename, 5.2 * cm, 0.12 * cm, width=1.45 * cm, height= 1.45 * cm)
+        c.drawImage(qr_filename, 5.2 * cm, 0.12 * cm, width=1.45 * cm, height= 1.00 * cm)
 
         #barcode_value = cliente  # Substitua pelo valor do código de barras desejado
         #barcode_code128 = barcode.code128.Code128(barcode_value, barHeight=15, humanReadable=True)
@@ -51,7 +51,7 @@ def criar_pdf(saida_pdf, titulo, cliente, pedido, transportadora, separador, agr
 
         c.drawString(2.0 * cm, 1.1 * cm, cliente)
         c.drawString(2.0 * cm, 0.8 * cm, pedido)
-        c.setFont("Helvetica-Bold", 6)
+        c.setFont("Helvetica-Bold", 5)
         c.drawString(2.5 * cm, 0.1 * cm, agrupamento)
 
         c.save()
