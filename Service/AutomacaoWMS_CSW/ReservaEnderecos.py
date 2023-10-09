b16417a5dc0b7a5ef5a156c472ce72f23bdc7505
luis-fe <luisengp@gmail.com>
2023-10-09 11:03:02 -0300

    update caixas

--- Service/AutomacaoWMS_CSW/ReservaEnderecos.py
+++ Service/AutomacaoWMS_CSW/ReservaEnderecos.py
@@ -210,7 +210,7 @@ def ReservaPedidosNaoRepostos(empresa, natureza, consideraSobra):
 
 
     tamanho = pedidoskuIteracao['codpedido'].size
-    pedidoskuIteracao['NecessidadeAcumulada'] = pedidoskuIteracao.groupby('produto')['Necessidade'].cumsum()
+    pedidoskuIteracao['NecessidadeAcumulada'] = pedidoskuIteracao.groupby('produto')['necessidade'].cumsum()
 
     pedidoskuIteracao = pedidoskuIteracao.reset_index(drop=False)
 
