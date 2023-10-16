3fe844381c7e7160caa3e3469cf375b293ce6b70
luis-fe <luisengp@gmail.com>
2023-10-16 11:44:38 -0300

    update caixas

--- Service/ReposicaoQualidade.py
+++ Service/ReposicaoQualidade.py
@@ -9,7 +9,7 @@ def ApontarTag(codbarras, Ncaixa, empresa):
     pesquisa = pd.read_sql('select p.codBarrasTag, p.codReduzido, p.codEngenharia as engenharia, '
                            ' (select i.nome from cgi.Item i WHERE i.codigo = p.codReduzido) as descricao, situacao, codNaturezaAtual as natureza, codEmpresa as codempresa, '
                            " (select s.corbase||'-'||s.nomecorbase  from tcp.SortimentosProduto s WHERE s.codempresa = 1 and s.codproduto = p.codEngenharia and s.codsortimento = p.codSortimento)" 
-                           ' as cor, (select t.descricao from tcp.Tamanhos t WHERE t.codempresa = 1 and t.sequencia = p.seqTamanho ) as tamanho'
+                           ' as cor, (select t.descricao from tcp.Tamanhos t WHERE t.codempresa = 1 and t.sequencia = p.seqTamanho ) as tamanho, p.numeroOP '
                            ' FROM Tcr.TagBarrasProduto p'
                            ' WHERE p.codBarrasTag = '+codbarras+' and p.codempresa ='+empresa,conn)
     conn.close()
