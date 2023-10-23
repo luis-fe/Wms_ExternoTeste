514bf7e17327535843df0ef2bbc7ab91bd177b88
luis-fe <luisengp@gmail.com>
2023-10-23 15:18:22 -0300

    update caixasd

--- Service/ReposicaoQualidade.py
+++ Service/ReposicaoQualidade.py
@@ -127,3 +127,17 @@ def IncrementarCaixa(endereco, dataframe):
             conn.close()
             return dataframe
 
+def PesquisarTagCsw(codbarras, empresa):
+    conn = ConexaoCSW.Conexao()
+    codbarras = "'" + codbarras + "'"
+
+    pesquisa = pd.read_sql(
+        'select p.codBarrasTag as codbarrastag , p.codReduzido as codreduzido, p.codEngenharia as engenharia, '
+        ' (select i.nome from cgi.Item i WHERE i.codigo = p.codReduzido) as descricao, situacao, codNaturezaAtual as natureza, codEmpresa as codempresa, '
+        " (select s.corbase||'-'||s.nomecorbase  from tcp.SortimentosProduto s WHERE s.codempresa = 1 and s.codproduto = p.codEngenharia and s.codsortimento = p.codSortimento)"
+        ' as cor, (select t.descricao from tcp.Tamanhos t WHERE t.codempresa = 1 and t.sequencia = p.seqTamanho ) as tamanho, p.numeroOP as numeroop '
+        ' FROM Tcr.TagBarrasProduto p'
+        ' WHERE p.codBarrasTag = ' + codbarras + ' and p.codempresa =' + empresa, conn)
+    conn.close()
+
+    return pesquisa
--- routes/ReposicaoQualidade_routes.py
+++ routes/ReposicaoQualidade_routes.py
@@ -57,3 +57,21 @@ def ConsultarCaixa():
             enderecos_dict[column_name] = row[column_name]
         enderecos_data.append(enderecos_dict)
     return jsonify(enderecos_data)
+
+@reposicao_qualidadeRoute.route('/api/PesquisarCodbarrastag', methods=['GET'])
+@token_required
+def PesquisarCodbarrastag():
+    empresa = request.args.get('empresa','1')
+    codbarras = request.args.get('codbarras','5')
+
+    FilaReposicaoOP = ReposicaoQualidade.PesquisarTagCsw(codbarras, empresa)
+    # Obtém os nomes das colunas
+    column_names = FilaReposicaoOP.columns
+    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
+    enderecos_data = []
+    for index, row in FilaReposicaoOP.iterrows():
+        enderecos_dict = {}
+        for column_name in column_names:
+            enderecos_dict[column_name] = row[column_name]
+        enderecos_data.append(enderecos_dict)
+    return jsonify(enderecos_data)
