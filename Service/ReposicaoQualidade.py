6f26c605325436ca541d3a08a633dfe8857b5f99
luis-fe <luisengp@gmail.com>
2023-10-24 11:51:50 -0300

    commit

--- Service/ReposicaoQualidade.py
+++ Service/ReposicaoQualidade.py
@@ -90,7 +90,7 @@ def EncontrarEPC(caixa):
     result.fillna('-', inplace=True)
 
     if result['mensagem'][0] == 'caixa vazia':
-        return pd.DataFrame({'mensagem':['caixa vazia']})
+        return pd.DataFrame({'mensagem':['caixa vazia'],'status':False})
     else:
         #Avaliar se a op da tag foi baixada
         result['mensagem'] = result.apply(lambda row: 'OP em estoque' if row['epc']!='-' else 'OP nao entrou em estoque',axis=1)
