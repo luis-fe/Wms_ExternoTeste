bfade8895d15402f28096551e4e14335bd70999b
luis-fe <luisengp@gmail.com>
2023-11-09 15:34:45 -0300

    commit

--- Service/ReposicaoQualidade.py
+++ Service/ReposicaoQualidade.py
@@ -241,6 +241,7 @@ def PesquisaOPSKU_tag(codbarras):
     consulta = pd.read_sql('select p.numeroOP, p.codReduzido FROM tcr.TagBarrasProduto p,'
                            '(select i.nome from cgi.Item i WHERE i.codigo = p.codReduzido) as descricao, '
                            "(select s.corbase||'-'||s.nomecorbase  from tcp.SortimentosProduto s WHERE s.codempresa = 1 and s.codproduto = p.codEngenharia and s.codsortimento = p.codSortimento) as cor"
+                           ' FROM tcr.TagBarrasProduto p '
                            'where codBarrasTag = '+codbarras,conn)
     conn.close()
     return consulta
