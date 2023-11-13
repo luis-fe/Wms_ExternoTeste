ec0644be85a1e824583295843ee532f61d18e707
luis-fe <luisengp@gmail.com>
2023-11-13 16:46:44 -0300

    commit

--- Service/ReposicaoQualidade.py
+++ Service/ReposicaoQualidade.py
@@ -299,7 +299,7 @@ def CaixasAbertasUsuario(empresa, codusuario):
     consulta = pd.merge(consulta, BipadoSKU, on=('codreduzido','numeroop'), how='left')
     consulta = Get_quantidadeOP_Sku(consulta, empresa)
     consulta['status']= consulta["bipado_sku_op"].astype(str)
-    consulta['total_pcs'] = consulta["total_pcs"].astype(str)
+    consulta['status']=  consulta['status'] + '/' + consulta["total_pcs"].astype(str)
 
 
     conn.close()
