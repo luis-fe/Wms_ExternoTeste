ac7e7af931d89d7b312f1744548f1d1acbc992a4
luis-fe <luisengp@gmail.com>
2023-11-14 14:58:02 -0300

    commit

--- Service/ReposicaoQualidade.py
+++ Service/ReposicaoQualidade.py
@@ -329,7 +329,7 @@ def Get_quantidadeOP_Sku(ops1, empresa, numeroop_ ='0'):
 
         if numeroop_ != '0':
             get['TotalOPGeral'] = get["total_pcs"].sum()
-            get['TotalOPGeral'] = get['TotalOPGeral'].round(0)
+            get['TotalOPGeral'] = get['TotalOPGeral'].atype(int)
         else:
             numeroop_ ='0'
         get = pd.merge(ops1, get , on='codreduzido', how='left')
