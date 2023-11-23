a7e1aba3ff570132a57c0cfed4d0acbbc9021a3c
luis-fe <luisengp@gmail.com>
2023-11-23 09:23:31 -0300

    commit

--- Service/necessidadeReposicaoModel.py
+++ Service/necessidadeReposicaoModel.py
@@ -46,11 +46,11 @@ def RelatorioNecessidadeReposicao():
 
     relatorioEndereço = relatorioEndereço[relatorioEndereço['engenharia']!= '-']
 
-    pedidos = pd.read_sql('select codpedido, produto as codreduzido, sum(necessidade) as necessidade  from "Reposicao".pedidossku p '
+    pedidos = pd.read_sql('select codpedido, produto as codreduzido, sum(necessidade) as necessidadePedido  from "Reposicao".pedidossku p '
                           "where p.necessidade > 0 and p.reservado = 'nao' "
                           " group by codpedido, produto", conn)
     #pedidos = pedidos.groupby('codreduzido')['codpedido'].agg(', '.join).reset_index()
-    pedidos = pedidos.groupby(['codreduzido']).agg({'codpedido': list, 'necessidade': list}).reset_index()
+    pedidos = pedidos.groupby(['codreduzido']).agg({'codpedido': list, 'necessidadePedido': list}).reset_index()
 
 
     relatorioEndereço = pd.merge(relatorioEndereço, pedidos, on='codreduzido', how='left')
