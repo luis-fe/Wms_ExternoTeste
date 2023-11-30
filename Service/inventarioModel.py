f145341571460afa7220424455495bc11dcadb91
luis-fe <luisengp@gmail.com>
2023-11-30 15:31:22 -0300

    commit

--- Service/inventarioModel.py
+++ Service/inventarioModel.py
@@ -354,10 +354,10 @@ def RelatorioInventario(dataInicio, dataFim, natureza, empresa):
 
 #    sql= sql[sql['situacao'] == 'finalizado']
 
-    sql = sql.groupby(['rua']).agg({
-        'codendereco': 'count',
-        'situacao':'count'
-    })
+    #sql = sql.groupby(['rua']).agg({
+     #   'codendereco': 'count',
+      #  'situacao':'count'
+   # })
 
 
     return sql
