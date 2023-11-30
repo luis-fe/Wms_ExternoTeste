ba628d8d429e64a6bcc165bf8e3e7646c8a31602
luis-fe <luisengp@gmail.com>
2023-11-30 10:54:45 -0300

    commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request, send_from_directory
 #from flask_cors import CORS
-import pandas as pd
+#import pandas as pd
 import os
 from functools import wraps
 import PediosApontamento
