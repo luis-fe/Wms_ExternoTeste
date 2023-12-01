c6b6e5c905fb48b6de310c562f3dc077c6e88940
luis-fe <luisengp@gmail.com>
2023-12-01 16:26:43 -0300

    commit

--- Service/inventarioModel.py
+++ Service/inventarioModel.py
@@ -528,7 +528,8 @@ def LimparTagsSaidaForaWms(situacao, empresa, natureza):
 
 
     consultar = pd.merge(consultar, FILA, on ='codbarrastag', how='right')
-    consultar.fillna('-', inplace=True)
+    consultar.fillna('-', inplace=True
+    consultar = consultar[consultar['Saida'] == '-']
 
     return consultar
 
