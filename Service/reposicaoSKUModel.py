ae7d18218fe4d3c617ad325bfe0d5e8398c9c5dc
luis-fe <luisengp@gmail.com>
2023-12-29 11:52:26 -0300

    commit

--- Service/reposicaoSKUModel.py
+++ Service/reposicaoSKUModel.py
@@ -6,7 +6,8 @@ import pandas as pd
 def detalhaSku(codreduzido, empresa,natureza):
     conn = ConexaoPostgreMPL.conexao()
     df_op2 = pd.read_sql('select "Endereco", "codreduzido", "descricao", count("codreduzido") as saldo, natureza '
-                   'from "Reposicao"."tagsreposicao" frt where "codreduzido" = ' +"'"+  codreduzido +"' and natureza = '"+natureza+"'"
+                   'from "Reposicao"."tagsreposicao" frt where "codreduzido" = ' +"'"+  codreduzido +"' "
+                                                                                                     #"and natureza = '"+natureza+"'" Aguardando ate o Sergio arrumar
                    ' group by "Endereco", "codreduzido", "descricao", natureza ', conn)
     if df_op2.empty:
         return pd.DataFrame({'Mensagem':[f'O reduzido {codreduzido} ainda nao foi reposto ou esta com as prateleiras vazias ']})
