2caf87c57e90af93923ad1d77e2ae4c8e84ebfd9
luis-fe <luisengp@gmail.com>
2024-01-08 11:14:16 -0300

    subprocess

--- Service/ReposicaoQualidade.py
+++ Service/ReposicaoQualidade.py
@@ -613,6 +613,7 @@ def DetalhaQuantidadeOP(empresa, numeroop):
                          ,'Tamanho':'3-Tam'}, inplace=True)
 
     novo['1- codSortimento'] = novo['1- codSortimento'] .astype(int)
+    novo['quantidade'] = novo['quantidade'].str.replace('.0','')
     novo = novo.sort_values(by='1- codSortimento', ascending=True)
     novo = novo
     data = {
