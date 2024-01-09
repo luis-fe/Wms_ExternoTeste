65414eda193af8a9786b44fcb160732568c7b67b
luis-fe <luisengp@gmail.com>
2024-01-09 09:11:45 -0300

    subprocess

--- Service/configuracoes/empresaConfigurada.py
+++ Service/configuracoes/empresaConfigurada.py
@@ -4,7 +4,7 @@ import ConexaoPostgreMPL
 
 def EmpresaEscolhida():
     conn = ConexaoPostgreMPL.conexao()
-    empresa = pd.read_sql('Select codempresa from "Reposicao".configuracao 'conn)
+    empresa = pd.read_sql('Select codempresa from "Reposicao".configuracao ',conn)
     conn.close()
 
     return empresa[0]
