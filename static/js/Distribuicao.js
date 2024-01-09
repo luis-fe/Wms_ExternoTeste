24bd967beb783fe10a7e53e8b74d4995cbc40242
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2024-01-09 14:10:18 -0300

    Update Distribuicao.js

--- static/js/Distribuicao.js
+++ static/js/Distribuicao.js
@@ -7,7 +7,7 @@ if (Empresa === "1") {
 } else if (Empresa === "4") {
     rootElement.classList.add('palheta-empresa-b');
 } else {
-    window.location.href = 'Login.html';
+    window.location.href = '/Login_Teste';
 }
 
 const ApiDistribuicaoMatriz = 'http://192.168.0.183:5000/api/FilaPedidos';
@@ -379,14 +379,14 @@ window.addEventListener('load', async () => {
     if (Empresa === "1") {
         if (VerificaLogin !== "Logado") {
 
-            window.location.href = 'Login.html';
+            window.location.href = '/Login_Teste';
         } else {
             await AtualizarDados(ApiRecarregarPedidosMatriz, '1');
             CarregarDados(ApiDistribuicaoMatriz);
         }
     } else if (Empresa === "4") {
         if (VerificaLogin !== "Logado") {
-            window.location.href = 'Login.html';
+            window.location.href = '/Login_Teste';
         } else {
             await AtualizarDados(ApiRecarregarPedidosFilial, '1');
             CarregarDados(ApiDistribuicaoFilial);
@@ -900,7 +900,7 @@ async function AtualizarDados(api, Empresa) {
     }
 }
 
-const linkSair = document.querySelector('.right-menu-item li a[href="Login.html"]');
+const linkSair = document.querySelector('.right-menu-item li a[href="/Login_Teste"]');
 
 linkSair.addEventListener("click" , async () => {
   localStorage.clear();
