5339a8b05cc567f8f5f6450689ec626498d3823c
luis-fe <luisengp@gmail.com>
2024-01-18 15:46:01 -0300

    mudanca de cor

--- Service/produtividadeModel.py
+++ Service/produtividadeModel.py
@@ -75,6 +75,22 @@ def ProdutividadeRepositores(dataInicial = '0', dataFInal ='0' , horarioInicial 
 
         Usuarios = pd.read_sql('Select codigo as usuario, nome from "Reposicao".cadusuarios ',conn)
         Usuarios['usuario'] = Usuarios['usuario'].astype(str)
+
+        ritmo2 = pd.read_sql(
+            'select count_tempo as ritmo, dia, usuario, data_intervalo_min as intervalo from "Reposicao"."Reposicao".ritimorepositor r '
+            ' WHERE r.dia >= %s and r.dia <= %s ', conn, params=(dataInicial, dataFInal,))
+        ritmo2['acum'] = ritmo2.groupby(['usuario', 'dia']).cumcount() + 1
+        ritmo2['acum'] = ritmo2['acum'] * (15 * 60)
+        ritmo2['ritmo'] = ritmo2.groupby(['usuario', 'dia'])['ritmo'].cumsum()
+        ritmo2['ritmo'] = ritmo2['acum'] / ritmo2['ritmo']
+        ritmo2 = ritmo2.groupby(['usuario', 'dia']).tail(1)
+
+        ritmo2 = ritmo2.groupby('usuario').agg({
+            'ritmo': 'mean'})
+        ritmo2['ritmo'] = ritmo2['ritmo'].round(2)
+
+        TagReposicao = pd.merge(TagReposicao, ritmo2, on='usuario', how='left')
+
         TagReposicao = pd.merge(TagReposicao, Usuarios,on='usuario',how='left')
         TagReposicao['qtde'] = TagReposicao['qtde'].astype(str)
         TagReposicao['qtde'] = TagReposicao['qtde'].str.replace(',', '.')
