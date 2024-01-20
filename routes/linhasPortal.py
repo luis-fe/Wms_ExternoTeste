bca0bd306c033d8a5f2b82384ba882b6aebd11d5
luis-fe <luisengp@gmail.com>
2024-01-20 20:07:12 -0300

    mudanca de cor

--- routes/linhasPortal.py
+++ routes/linhasPortal.py
@@ -57,8 +57,8 @@ def NovaLinha():
     data = request.get_json()
     linha = data['linha']
     oper1 = data['operador1']
-    oper2 = data('operador2','-')
-    oper3 = data('operador3','-')
+    oper2 = data['operador2','-']
+    oper3 = data['operador3','-']
     linhas = LinhasPortal.CadastrarLinha(linha,oper1, oper2, oper3)
     # Obtém os nomes das colunas
 
