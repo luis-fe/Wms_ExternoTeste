4f17f1d967ee382250142f564e9ea830f8c9435a
luis-fe <luisengp@gmail.com>
2024-01-20 13:14:42 -0300

    mudanca de cor

--- Service/AutomacaoWMS_CSW/AtualizarFilaGarantia.py
+++ Service/AutomacaoWMS_CSW/AtualizarFilaGarantia.py
@@ -25,3 +25,39 @@ def AtualizaFilaGarantia():
     ConexaoPostgreMPL.Funcao_InserirOFF(consulta, consulta.size, 'filareposicaoof', 'replace')
 
     return pd.DataFrame([{'Status': True, 'mensagem':'tags atualizadas na Garantia com sucesso !'}])
+
+
+def AtualizacaoFilaOFF_op(op):
+    emp = empresaConfigurada.EmpresaEscolhida()
+    numeroop = op
+    op = "'"+op+"'"
+
+    conn = ConexaoCSW.Conexao()
+
+    consulta = pd.read_sql(
+        'SELECT p.codBarrasTag as codbarrastag , p.codReduzido as codreduzido, p.codEngenharia as engenharia,'
+        '(select i.nome from cgi.Item i WHERE i.codigo = p.codReduzido) as descricao, situacao, codNaturezaAtual as natureza, codEmpresa as codempresa,'
+        " (select s.corbase||'-'||s.nomecorbase  from tcp.SortimentosProduto s WHERE s.codempresa = 1 and s.codproduto = p.codEngenharia and s.codsortimento = p.codSortimento)"
+        ' as cor, (select t.descricao from tcp.Tamanhos t WHERE t.codempresa = 1 and t.sequencia = p.seqTamanho ) as tamanho, p.numeroOP as numeroop'
+        ' from Tcr.TagBarrasProduto p WHERE p.codEmpresa = ' + emp + ' and '
+                                                                     ' p.numeroOP = '+op,
+        conn)
+
+    conn.close()
+
+    conn2 = ConexaoPostgreMPL.conexao()
+
+    delete = 'Delete from "Reposciao".off.filareposicaoof where numeroop = %s '
+
+    cursor = conn2.cursor()
+    cursor.execute(delete
+                   , (
+                       numeroop,))
+    conn2.commit()
+    cursor.close()
+
+    conn2.close()
+
+    ConexaoPostgreMPL.Funcao_InserirOFF(consulta, consulta.size, 'filareposicaoof', 'append')
+
+    return pd.DataFrame([{'Status': True, 'mensagem':f'tags da op {numeroop} atualizadas na Garantia com sucesso !'}])
--- routes/automacaoWMS_CSW.py
+++ routes/automacaoWMS_CSW.py
@@ -226,4 +226,24 @@ def AtualizacaoFilaOFF():
 
     return jsonify(pedidos_data)
 
+@AutomacaoWMS_CSW_routes.route('/api/AtualizacaoFilaOFF_op', methods=['GET'])
+@token_required
+def AtualizacaoFilaOFF_op():
+    op = request.args.get('op', '-')
+    TagReposicao = AtualizarFilaGarantia.AtualizacaoFilaOFF_op(op)
+
+
+
+
+    # Obtém os nomes das colunas
+    column_names = TagReposicao.columns
+    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
+    pedidos_data = []
+    for index, row in TagReposicao.iterrows():
+        pedidos_dict = {}
+        for column_name in column_names:
+            pedidos_dict[column_name] = row[column_name]
+        pedidos_data.append(pedidos_dict)
+
+    return jsonify(pedidos_data)
 
