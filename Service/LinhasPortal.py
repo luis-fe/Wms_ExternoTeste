03f19d9b76a4601e71b41f3ba734716cb2ee6802
luis-fe <luisengp@gmail.com>
2024-01-22 07:38:26 -0300

    mudanca de cor

--- Service/LinhasPortal.py
+++ Service/LinhasPortal.py
@@ -23,104 +23,111 @@ def RetornarNomeLinha(linha):
 
     conn.close()
 
-    return pd.DataFrame([{'OperadoresLinha':linhas['operador1'][0]+'/'+linhas['operador2'][0]+'/'+linhas['operador3'][0]}])
+    if not linha.empety:
+        return pd.DataFrame([{'OperadoresLinha':linhas['operador1'][0]+'/'+linhas['operador2'][0]+'/'+linhas['operador3'][0],'status':True}])
+    else:
+        return pd.DataFrame([{'Mesamge':'Nao existe essa linha informado', 'status':False}])
 
 def CadastrarLinha(nomeLinha, operador1, operador2, operador3):
-    conn = ConexaoPostgreMPL.conexao()
+
+    consularLinha = RetornarNomeLinha(nomeLinha)
+    if consularLinha['status'] == True:
+        conn = ConexaoPostgreMPL.conexao()
 
 
-    if operador3 != '-':
-        insertInto = 'insert into "Reposicao".off."linhapadrado"' \
-                 ' ("Linha",operador1, operador2, operador3) values (%s, %s, %s , %s)'
-        cursor = conn.cursor()
-        try:
-            cursor.execute(insertInto, (nomeLinha, operador1, operador2, operador3))
-            conn.commit()
-            mensagem = pd.DataFrame([{'Mensagem': 'Linha cadastrado com sucesso'}])
+        if operador3 != '-':
+            insertInto = 'insert into "Reposicao".off."linhapadrado"' \
+                     ' ("Linha",operador1, operador2, operador3) values (%s, %s, %s , %s)'
+            cursor = conn.cursor()
+            try:
+                cursor.execute(insertInto, (nomeLinha, operador1, operador2, operador3))
+                conn.commit()
+                mensagem = pd.DataFrame([{'Mensagem': 'Linha cadastrado com sucesso'}])
 
-        except errors.ForeignKeyViolation as e:
-            # Se uma exceção de violação de chave estrangeira ocorrer, imprima a mensagem de erro ou faça o que for necessário
+            except errors.ForeignKeyViolation as e:
+                # Se uma exceção de violação de chave estrangeira ocorrer, imprima a mensagem de erro ou faça o que for necessário
+                cursor.close()
+                conn.close()
+                print(f"Erro inesperado nome operador1: {e}")
+
+                mensagem = pd.DataFrame(
+                    [{'Mensagem': f'{e}'}])
+
+
+            except Exception as e:
+                # Lide com outras exceções aqui, se necessário
+                cursor.close()
+                conn.close()
+                print(f"Erro inesperado: {e}")
+                mensagem = pd.DataFrame(
+                    [{'Mensagem': f'NÃO EXISTE O NOME DO OPERADOR 1: {operador1} no cadastro de usuarios do portal !'}])
+
             cursor.close()
             conn.close()
-            print(f"Erro inesperado nome operador1: {e}")
-
-            mensagem = pd.DataFrame(
-                [{'Mensagem': f'{e}'}])
+            return mensagem
 
 
-        except Exception as e:
-            # Lide com outras exceções aqui, se necessário
+        elif operador3 == '-' and operador2 != '-':
+            insertInto = 'insert into "Reposicao".off."linhapadrado" ' \
+                         '("Linha", operador1, operador2) values (%s, %s, %s)'
+            cursor = conn.cursor()
+            try:
+                cursor.execute(insertInto, (nomeLinha, operador1, operador2))
+                conn.commit()
+                mensagem = pd.DataFrame([{'Mensagem': 'Linha cadastrado com sucesso'}])
+
+            except errors.ForeignKeyViolation as e:
+                # Se uma exceção de violação de chave estrangeira ocorrer, imprima a mensagem de erro ou faça o que for necessário
+                cursor.close()
+                conn.close()
+                print(f"Erro inesperado nome operador1: {e}")
+
+                mensagem = pd.DataFrame(
+                    [{'Mensagem': f'{e}'}])
+
+
+            except Exception as e:
+                # Lide com outras exceções aqui, se necessário
+                cursor.close()
+                conn.close()
+                print(f"Erro inesperado: {e}")
+                mensagem = pd.DataFrame(
+                    [{'Mensagem': f'NÃO EXISTE O NOME DO OPERADOR 1: {operador1} no cadastro de usuarios do portal !'}])
+
             cursor.close()
             conn.close()
-            print(f"Erro inesperado: {e}")
-            mensagem = pd.DataFrame(
-                [{'Mensagem': f'NÃO EXISTE O NOME DO OPERADOR 1: {operador1} no cadastro de usuarios do portal !'}])
-
-        cursor.close()
-        conn.close()
-        return mensagem
+            return mensagem
 
 
-    elif operador3 == '-' and operador2 != '-':
-        insertInto = 'insert into "Reposicao".off."linhapadrado" ' \
-                     '("Linha", operador1, operador2) values (%s, %s, %s)'
-        cursor = conn.cursor()
-        try:
-            cursor.execute(insertInto, (nomeLinha, operador1, operador2))
-            conn.commit()
-            mensagem = pd.DataFrame([{'Mensagem': 'Linha cadastrado com sucesso'}])
 
-        except errors.ForeignKeyViolation as e:
-            # Se uma exceção de violação de chave estrangeira ocorrer, imprima a mensagem de erro ou faça o que for necessário
+        elif operador2 == '-':
+            insertInto = 'insert into "Reposicao".off."linhapadrado" ' \
+                         '("Linha", operador1) values (%s, %s)'
+            cursor = conn.cursor()
+            try:
+                cursor.execute(insertInto, (nomeLinha, operador1))
+                conn.commit()
+                mensagem = pd.DataFrame([{'Mensagem': 'Linha cadastrado com sucesso'}])
+
+            except errors.ForeignKeyViolation as e:
+                # Se uma exceção de violação de chave estrangeira ocorrer, imprima a mensagem de erro ou faça o que for necessário
+                cursor.close()
+                conn.close()
+                print(f"Erro inesperado nome operador1: {e}")
+
+                mensagem = pd.DataFrame([{'Mensagem': f'NÃO EXISTE O NOME DO OPERADOR 1: {operador1} no cadastro de usuarios do portal !'}])
+
+
+            except Exception as e:
+                # Lide com outras exceções aqui, se necessário
+                cursor.close()
+                conn.close()
+                print(f"Erro inesperado: {e}")
+                mensagem = pd.DataFrame(
+                    [{'Mensagem': f'NÃO EXISTE O NOME DO OPERADOR 1: {operador1} no cadastro de usuarios do portal !'}])
+
             cursor.close()
             conn.close()
-            print(f"Erro inesperado nome operador1: {e}")
-
-            mensagem = pd.DataFrame(
-                [{'Mensagem': f'{e}'}])
-
-
-        except Exception as e:
-            # Lide com outras exceções aqui, se necessário
-            cursor.close()
-            conn.close()
-            print(f"Erro inesperado: {e}")
-            mensagem = pd.DataFrame(
-                [{'Mensagem': f'NÃO EXISTE O NOME DO OPERADOR 1: {operador1} no cadastro de usuarios do portal !'}])
-
-        cursor.close()
-        conn.close()
-        return mensagem
-
-
-
-    elif operador2 == '-':
-        insertInto = 'insert into "Reposicao".off."linhapadrado" ' \
-                     '("Linha", operador1) values (%s, %s)'
-        cursor = conn.cursor()
-        try:
-            cursor.execute(insertInto, (nomeLinha, operador1))
-            conn.commit()
-            mensagem = pd.DataFrame([{'Mensagem': 'Linha cadastrado com sucesso'}])
-
-        except errors.ForeignKeyViolation as e:
-            # Se uma exceção de violação de chave estrangeira ocorrer, imprima a mensagem de erro ou faça o que for necessário
-            cursor.close()
-            conn.close()
-            print(f"Erro inesperado nome operador1: {e}")
-
-            mensagem = pd.DataFrame([{'Mensagem': f'NÃO EXISTE O NOME DO OPERADOR 1: {operador1} no cadastro de usuarios do portal !'}])
-
-
-        except Exception as e:
-            # Lide com outras exceções aqui, se necessário
-            cursor.close()
-            conn.close()
-            print(f"Erro inesperado: {e}")
-            mensagem = pd.DataFrame(
-                [{'Mensagem': f'NÃO EXISTE O NOME DO OPERADOR 1: {operador1} no cadastro de usuarios do portal !'}])
-
-        cursor.close()
-        conn.close()
-        return mensagem
-
+            return mensagem
+    else:
+        return consularLinha
