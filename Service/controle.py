b44e20186bf7e5b5fd97df9b325a1ac0dc4df508
luis-fe <luisengp@gmail.com>
2024-03-08 14:46:06 -0300

    atualizacap

--- Service/controle.py
+++ Service/controle.py
@@ -31,6 +31,7 @@ def salvar(rotina, ip,datahoraInicio):
     # Obtém a diferença total em segundos
     diferenca_total_segundos = diferenca.total_seconds()
     milissegundos = diferenca.microseconds / 1000
+    print(milissegundos)
     tempoProcessamento = float(diferenca_total_segundos + milissegundos /1000)
 
 
