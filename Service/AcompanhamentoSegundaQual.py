a3b09f58f665156f845fc43ec53b3b1981ab6d79
luis-fe <luisengp@gmail.com>
2024-03-11 17:12:54 -0300

    atualizacap

--- Service/AcompanhamentoSegundaQual.py
+++ Service/AcompanhamentoSegundaQual.py
@@ -31,10 +31,20 @@ def TagSegundaQualidade(iniVenda, finalVenda):
 
 
 def MotivosAgrupado(iniVenda, finalVenda):
+    iniVenda = iniVenda[6:] + "-" + iniVenda[3:5] + "-" + iniVenda[:2]
+    finalVenda = finalVenda[6:] + "-" + finalVenda[3:5] + "-" + finalVenda[:2]
+    conn = ConexaoCSW.Conexao()
 
-    tags = TagSegundaQualidade(iniVenda,finalVenda)
-    #Agrupamento do quantitativo
-    tags['qtde'] = tags['qtde'].astype(int)
+    tags = pd.read_sql(BuscasAvancadas.TagsSegundaQualidadePeriodo(iniVenda, finalVenda), conn)
+    motivos = pd.read_sql(BuscasAvancadas.Motivos(), conn)
+    tags['motivo2Qualidade'] = tags['motivo2Qualidade'].astype(str)
+    motivos['motivo2Qualidade'] = motivos['motivo2Qualidade'].astype(str)
+
+    tags = pd.merge(tags, motivos, on='motivo2Qualidade', how='left')
+
+    tags['motivo2Qualidade'] = tags['nome']
+    tags['qtde'] = 1
+    conn.close()
 
     Agrupamento = tags.groupby('motivo2Qualidade')['qtde'].sum().reset_index()
     Agrupamento = Agrupamento.sort_values(by='qtde', ascending=False,
