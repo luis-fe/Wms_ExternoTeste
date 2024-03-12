c3914ad9c11762f8590571b1e4cc8b890a3e5ffb
luis-fe <luisengp@gmail.com>
2024-03-12 13:22:29 -0300

    atualizacap

--- Service/AcompanhamentoSegundaQual.py
+++ Service/AcompanhamentoSegundaQual.py
@@ -22,7 +22,6 @@ def TagSegundaQualidade(iniVenda, finalVenda):
     PecasBaixadas = pd.read_sql(BuscasAvancadas.OpsBaixadas(iniVenda,finalVenda), conn)
     OpsFaccinista = pd.read_sql(BuscasAvancadas.OpsBaixadasFaccionista(iniVenda,finalVenda), conn)
 
-
     tags = pd.merge(tags,OpsFaccinista,on='numeroOP', how='left')
 
     TotalPCsBaixadas = PecasBaixadas['qtdMovto'].sum()
