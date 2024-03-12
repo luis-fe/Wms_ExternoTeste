c46bc7a77c1322ac24327d21ccb20c78c0a9de48
luis-fe <luisengp@gmail.com>
2024-03-12 09:40:26 -0300

    atualizacap

--- Service/AcompanhamentoSegundaQual.py
+++ Service/AcompanhamentoSegundaQual.py
@@ -27,7 +27,6 @@ def TagSegundaQualidade(iniVenda, finalVenda):
 
     TotalPecas = tags['qtde'].sum()
     data = {
-
         '1- Peças com Motivo de 2Qual.': TotalPecas ,
         '2- Total Peças Baixadas periodo': TotalPCsBaixadas,
         '4- Detalhamento ': tags.to_dict(orient='records')
