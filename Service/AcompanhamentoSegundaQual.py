395dfef6c38ccde7d66d87b075a0fcaec9b2cfd9
luis-fe <luisengp@gmail.com>
2024-03-27 12:36:11 -0300

    UPDATE ITENS A SEREM SUBSTITUIDOS

--- Service/AcompanhamentoSegundaQual.py
+++ Service/AcompanhamentoSegundaQual.py
@@ -62,11 +62,13 @@ def TagSegundaQualidade(iniVenda, finalVenda):
 
     tags = pd.merge(tags,fasesInternas,on=['OPpai','nomeOrigem'], how='left')
 
+    tags.fillna('-',inplace=True)
+
+
     tags['nomeFaccicionistaCostura'] = tags.apply(lambda row: row['nomeInterno'] if row['nomeFaccicionistaCostura'] == '-' else row['nomeFaccicionistaCostura'], axis=1)
     tags.drop('nomeInterno', axis=1, inplace=True)
 
 
-    tags.fillna('-',inplace=True)
 
     TotalPCsBaixadas = PecasBaixadas['qtdMovto'].sum()
 
