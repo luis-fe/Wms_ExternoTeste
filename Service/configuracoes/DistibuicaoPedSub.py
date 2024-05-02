01ea7efdb9e690f3f3fa72ab92cb742f97733b70
luis-fe <luisengp@gmail.com>
2024-05-02 16:02:26 -0300

    testando a implantacao do modulo de distribuicao de pedidos: oferecendo a restricao Padrao e o saldoLiquidoTotal2

--- Service/configuracoes/DistibuicaoPedSub.py
+++ Service/configuracoes/DistibuicaoPedSub.py
@@ -69,6 +69,7 @@ join (select "Endereco", max(resticao) as "Restricao" from "Reposicao"."Reposica
 
     consulta = pd.merge(consulta,SaldoPorRestricao2,on=['engenharia','cor'], how='left')
     consulta = pd.merge(consulta,consultaSaldoRestricaoProduto,on=['produto','Restricao Sugerida'], how='left')
+    consulta['SaldoEndereco'].consulta(0, inplace=True)
 
     consulta = consulta.apply(lambda row: 'MUDAR' if row['Restricao'] != row['Restricao Sugerida'] and row['SaldoEndereco'] > 0 else 'MANTER',axis=1    )
     return consulta
