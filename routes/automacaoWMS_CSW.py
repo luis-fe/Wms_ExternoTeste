cbef0ab1eb6043c191098c73fd06a1cbc4d740e9
luis-fe <luisengp@gmail.com>
2024-05-23 15:18:54 -0300

    corrigido de suituacao, na api registro de inventario

--- routes/automacaoWMS_CSW.py
+++ routes/automacaoWMS_CSW.py
@@ -132,7 +132,10 @@ def RecarregarPedidos():
     ReservaEnderecos.ReservaPedidosNaoRepostos(empresa,natureza,bool(consideraSobra),ordem,int(3),'Substitutos')
     ReservaEnderecos.ReservaPedidosNaoRepostos(empresa,natureza,bool(True),'desc',int(3),'')
     necessidadeReposicaoModel.RelatorioNecessidadeReposicaoDisponivel(empresa, natureza)
-    DistibuicaoPedSub.PedidosSkuEspecial()
+    if empresa == '1':
+        DistibuicaoPedSub.PedidosSkuEspecial()
+    else:
+        print(empresa)
 
 
 
