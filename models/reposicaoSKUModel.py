10cad5228c52bfe13800c7a36d082d638194d985
luis-fe <luisengp@gmail.com>
2024-06-04 15:33:14 -0300

    implantacao

--- models/reposicaoSKUModel.py
+++ models/reposicaoSKUModel.py
@@ -5,10 +5,19 @@ import pandas as pd
 
 def detalhaSku(codreduzido, empresa,natureza):
     conn = ConexaoPostgreMPL.conexao()
-    df_op2 = pd.read_sql('select "Endereco", "codreduzido", "descricao", count("codreduzido") as saldo, natureza '
-                   'from "Reposicao"."tagsreposicao" frt where "codreduzido" = ' +"'"+  codreduzido +"' "
-                                                                                                     "and natureza = '"+natureza+"'"# Aguardando ate o Sergio arrumar
-                   ' group by "Endereco", "codreduzido", "descricao", natureza ', conn)
+    query = """
+    SELECT "Endereco", "codreduzido", "descricao", COUNT("codreduzido") AS saldo, natureza
+    FROM "Reposicao"."tagsreposicao"
+    WHERE "codreduzido" = %s
+    AND natureza = %s
+    GROUP BY "Endereco", "codreduzido", "descricao", natureza
+    """
+
+
+    df_op2 = pd.read_sql(query, conn, params=(codreduzido, natureza))
+
+
+
     if df_op2.empty:
         return pd.DataFrame({'Mensagem':[f'O reduzido {codreduzido} ainda nao foi reposto ou esta com as prateleiras vazias ']})
     else:
