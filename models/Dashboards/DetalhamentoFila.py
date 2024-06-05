9b5963b31056a4d12201a49b28f6a935cca16db0
luis-fe <luisengp@gmail.com>
2024-06-05 17:56:51 -0300

    implantacao

--- models/Dashboards/DetalhamentoFila.py
+++ models/Dashboards/DetalhamentoFila.py
@@ -5,13 +5,20 @@ import ConexaoPostgreMPL
 
 def detalhaFila():
     detalalhaTags = """
-    select f.numeroop, codreduzido , count(codbarrastag) as pcs  from "Reposicao"."Reposicao".filareposicaoportag f 
-    group by numeroop, codreduzido  
+    select f.numeroop, codreduzido , descricao, count(codbarrastag) as pcs  from "Reposicao"."Reposicao".filareposicaoportag f 
+    group by numeroop, codreduzido, descricao  
     order by count(codbarrastag) desc
+    """
+
+    caixa = """
+    select rq.caixa , rq.numeroop , rq.codreduzido , count(rq.codbarrastag) pc  from "Reposicao"."off".reposicao_qualidade rq 
+inner join "Reposicao"."Reposicao".filareposicaoportag f on f.codbarrastag = rq.codbarrastag
+group by rq.caixa , rq.numeroop , rq.codreduzido
     """
 
     with ConexaoPostgreMPL.conexao() as conn:
         detalalhaTags = pd.read_sql(detalalhaTags, conn)
+        caixa = pd.read_sql(caixa, conn)
 
     return detalalhaTags
 
