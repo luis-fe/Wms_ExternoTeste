7b50b8362f9a5bcde1bf640c1a9d2a7968c419cb
luis-fe <luisengp@gmail.com>
2024-06-14 14:11:55 -0300

    arrumando dados

--- models/Dashboards/DetalhamentoFila.py
+++ models/Dashboards/DetalhamentoFila.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+import ConexaoCSW
 import ConexaoPostgreMPL
 
 
@@ -17,6 +18,23 @@ inner join "Reposicao"."Reposicao".filareposicaoportag f on f.codbarrastag = rq.
 group by rq.caixa , rq.numeroop , rq.codreduzido
     """
 
+    sqlCsw = """
+    SELECT top 100000 numeroOP as numeroop ,dataFim, L.descricao as descOP FROM tco.OrdemProd o 
+inner join tcl.Lote L on L.codempresa = o.codempresa and L.codlote = o.codlote
+WHERE o.codEmpresa = 1 
+and o.situacao = 2 
+order by numeroOP desc
+    """
+
+    with ConexaoCSW.Conexao() as conn:
+        with conn.cursor() as cursor_csw:
+            # Executa a primeira consulta e armazena os resultados
+            cursor_csw.execute(sqlCsw)
+            colunas = [desc[0] for desc in cursor_csw.description]
+            rows = cursor_csw.fetchall()
+            dadosOP = pd.DataFrame(rows, columns=colunas)
+            del rows
+
     with ConexaoPostgreMPL.conexao() as conn:
         detalalhaTags = pd.read_sql(detalalhaTags, conn, params=(empresa, natureza))
         caixapd = pd.read_sql(caixa, conn)
@@ -26,6 +44,8 @@ group by rq.caixa , rq.numeroop , rq.codreduzido
         lambda x: ', '.join(x['caixa'].astype(str) + ':' + x['pc'].astype(str))).reset_index(name='caixas')
 
     detalalhaTags = pd.merge(detalalhaTags, caixa ,on=['numeroop', 'codreduzido'],how='left')
+
+    detalalhaTags = pd.merge(detalalhaTags,dadosOP,on='numeroop', how='left')
     detalalhaTags.fillna('-',inplace=True)
 
     data = { '1.0- Total Peças Fila': f'{detalalhaTags["pcs"].sum()} pcs',
