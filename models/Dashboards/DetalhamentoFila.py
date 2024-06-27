7655abb786b27e37e9b3133142c46e80c8ea8c9d
luis-fe <luisengp@gmail.com>
2024-06-27 12:07:29 -0300

    atualizacao

--- models/Dashboards/DetalhamentoFila.py
+++ models/Dashboards/DetalhamentoFila.py
@@ -184,7 +184,7 @@ def ValidandoTracoOP():
 def DetalhaTagsNumeroOPReduzido(numeroop, codreduzido, codempresa, natureza):
     sql = """
     select f.codbarrastag , f.epc, f.numeroop, f."DataHora", f.codreduzido  from "Reposicao"."Reposicao".filareposicaoportag f 
-where numeroop = %s and codreduzido = %s and status_fila is null and f.codempresa =  %s and f.codnaturezaatual =  %s
+where numeroop = %s and codreduzido = %s and (status_fila is null or status_fila = 'Devolucao' ) and f.codempresa =  %s and f.codnaturezaatual =  %s
     """
 
     conn = ConexaoPostgreMPL.conexaoEngine()
