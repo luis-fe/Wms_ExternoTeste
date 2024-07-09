6c831e9eaed683d7cdc85eff593d30b14c45f07c
luis-fe <luisengp@gmail.com>
2024-07-09 09:08:19 -0300

    SubstitutosSkuOP

--- models/Dashboards/ConsultaEstoque.py
+++ models/Dashboards/ConsultaEstoque.py
@@ -59,7 +59,7 @@ def ConsultaEnderecoReposto(natureza, codreduzido = '-', codengenharia = '-', nu
 
 
     else:
-        consulta = 'Select  "Endereco", codreduzido, t.engenharia , count(codbarrastag) as saldo, descricao as nome, cor as desc_cor, tam as desc_tam from "Reposicao".tagsreposicao t ' \
+        consulta = 'Select  "Endereco", codreduzido, t.engenharia , count(codbarrastag) as saldo, descricao as nome, cor as desc_cor, tamanho as desc_tam from "Reposicao".tagsreposicao t ' \
                    ' where natureza = %s '\
                 'and natureza = %s ' \
                    'group by "Endereco", codreduzido, engenharia, descricao, cor, tamanho ' \
