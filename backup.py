f4ccd37fa220f86c79f7ea02a88951f59064c309
luis-fe <luisengp@gmail.com>
2024-07-10 17:38:24 -0300

    SubstitutosSkuOP

---
+++ backup.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import gc
+import psycopg2
+from sqlalchemy import create_engine
+from models.configuracoes import  empresaConfigurada
+
+
+def Backup():
+
+    bac = pd.read_csv('tagsreposicao.csv',sep=';')
+
+    Funcao_Inserir(bac,75000,'tagsreposicao','append')
+
+def Funcao_Inserir (df_tags, tamanho,tabela, metodo):
+    # Configurações de conexão ao banco de dados
+    database = "Reposicao"
+    user = "postgres"
+    password = "Master100"
+
+    host = "192.168.0.183"
+
+    port = "5432"
+
+# Cria conexão ao banco de dados usando SQLAlchemy
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{database}')
+
+    # Inserir dados em lotes
+    chunksize = tamanho
+    for i in range(0, len(df_tags), chunksize):
+        df_tags.iloc[i:i + chunksize].to_sql(tabela, engine, if_exists=metodo, index=False , schema='Reposicao')
+
+
+Backup()
