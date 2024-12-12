20d9e64c42101f4de775907028e48318d15ac970
luis-fe <luisengp@gmail.com>
2024-12-12 17:16:30 -0300

    tela de Acessso 2

--- models/Perfil.py
+++ models/Perfil.py
@@ -54,8 +54,60 @@ class Perfil ():
         '''metodo utilizado para inserir tela de acesso ao perfil'''
 
 
-    def cadastrarPerfil(self):
+        delete = """
+        delete from "Reposicao"."TelaAcessoPerfil" where "codPerfil" = %s
+        
+        """
+
+        with ConexaoPostgreMPL.conexao() as conn:
+            with conn.cursor() as curr:
+                curr.execute(delete, (self.codPerfil,))
+                conn.commit()
+
+        for t in self.telaAcesso:
+
+            self.nomeTela = t
+
+            insert = """
+            insert into 
+                "Reposicao"."Reposicao"."TelaAcessoPerfil" ("codPerfil", "nomeTela")
+                values (%s , %s )
+            """
+
+            with ConexaoPostgreMPL.conexao() as conn:
+                with conn.cursor() as curr:
+                    curr.execute(insert,(self.codPerfil, self.nomeTela))
+                    conn.commit()
+
+
+    def cadastrarOuAtualizarPerfil(self):
         '''Metodo utilizado para cadastrar Perfil '''
+
+        verfica = self.consultarPerfilPorCodigo()
+
+        if verfica.empty:
+
+
+            insert = '''
+            insert into 
+                "Reposicao"."Pefil" ("codPerfil","nomePerfil")
+            values ( %s, %s )
+            '''
+
+            with ConexaoPostgreMPL.conexao() as conn:
+                with conn.cursor() as curr:
+                    curr.execute(insert,(self.codPerfil, self.nomePerfil))
+                    conn.commit()
+
+            self.inserirTelaAcessoAoPerfil()
+
+            return pd.DataFrame([{'status':True, 'Mensagagem':f'Perfil {self.codPerfil}-{self.nomePerfil} inserido com sucesso !'}])
+
+        else:
+            self.updatePerfil()
+            self.inserirTelaAcessoAoPerfil()
+            return pd.DataFrame([{'status':True, 'Mensagagem':f'Perfil {self.codPerfil}-{self.nomePerfil} atualziado com sucesso !'}])
+
 
 
     def exclussaoDePerfil(self):
@@ -64,3 +116,36 @@ class Perfil ():
 
     def updatePerfil(self):
         '''Metodo utilizado para update do Perfil '''
+
+        update = """
+        update 
+            "Reposicao"."Pefil" 
+        set 
+            "nomePerfil" = %s
+        where
+            "codPerfil" = %s  
+        """
+
+        with ConexaoPostgreMPL.conexao() as conn:
+            with conn.cursor() as curr:
+                curr.execute(update,(self.nomePerfil, self.codPerfil))
+                conn.commit()
+
+
+    def consultarPerfilPorCodigo(self):
+        '''Metodo utilizado para consultar o perfil pelo codigo '''
+
+        sql = """
+                select
+	        "codPerfil",
+	        "nomePerfil"
+        from
+	        "Reposicao"."Reposicao"."Pefil" p 
+	    where 
+	        "codPerfil" = %s
+        """
+
+        conn = ConexaoPostgreMPL.conexaoEngine()
+        consulta = pd.read_sql(sql,conn,params=(self.codPerfil,))
+
+        return consulta
--- routes/Pefil.py
+++ routes/Pefil.py
@@ -32,3 +32,26 @@ def get_consultaPerfil():
             consulta_dict[column_name] = row[column_name]
         consulta_data.append(consulta_dict)
     return jsonify(consulta_data)
+
+@Perfil_routes.route('/api/cadastrarOuAtualizarPerfil', methods=['POST'])
+@token_required
+def post_cadastrarOuAtualizarPerfil():
+
+    # Obtenha os dados do corpo da requisição
+    novo_Tela = request.get_json()
+    # Extraia os valores dos campos do novo usuário
+    codPerfil = novo_Tela.get('codPerfil')
+    nomePerfil = novo_Tela.get('nomePerfil')
+    telasAcesso = novo_Tela.get('telasAcesso')
+
+    consulta = Perfil.Perfil(codPerfil, nomePerfil, telasAcesso).cadastrarOuAtualizarPerfil()
+    # Obtém os nomes das colunas
+    column_names = consulta.columns
+    # Monta o dicionário com os cabeçalhos das colunas e os valores correspondentes
+    consulta_data = []
+    for index, row in consulta.iterrows():
+        consulta_dict = {}
+        for column_name in column_names:
+            consulta_dict[column_name] = row[column_name]
+        consulta_data.append(consulta_dict)
+    return jsonify(consulta_data)
